c0b5c446d0944d4bcaae280275eec6d9fb6acac8
Gabriel Gizzi <gabrielgizzi07@gmail.com>
2026-04-17 18:59:15 -0300

    Readded setup()

--- lua/vim-be-better/init.lua
+++ lua/vim-be-better/init.lua
@@ -3,13 +3,9 @@ local menu = require("vim-be-better.menu")
 -- Module object
 local M = {}
 
--- M.setup = function()
---     vim.api.nvim_create_user_command(
---         "VimBeBetter",
---         menu.open(),
---         { nargs = 0 }
---     )
--- end
+M.setup = function()
+    print("vim-be-better loaded")
+end
 
 M.open = menu.open
 
