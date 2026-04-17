8c1897f15b73474692dbe74e5a4d46de4c1a0d7b
Gabriel Gizzi <gabrielgizzi07@gmail.com>
2026-04-17 18:55:45 -0300

    Now it's working?...

--- lua/vim-be-better/init.lua
+++ lua/vim-be-better/init.lua
@@ -3,13 +3,13 @@ local menu = require("vim-be-better.menu")
 -- Module object
 local M = {}
 
-M.setup = function()
-    vim.api.nvim_create_user_command(
-        "VimBeBetter",
-        menu.open(),
-        { nargs = 0 }
-    )
-end
+-- M.setup = function()
+--     vim.api.nvim_create_user_command(
+--         "VimBeBetter",
+--         menu.open(),
+--         { nargs = 0 }
+--     )
+-- end
 
 M.open = menu.open
 
--- plugin/vim-be-better.vim
+++ plugin/vim-be-better.vim
@@ -2,3 +2,5 @@ if exists("g_loaded_vimbebetter")
     finish
 endif
 let g:loaded_vimbebetter = 1
+
+command! -nargs=0 VimBeBetter lua require("vim-be-better").open()
